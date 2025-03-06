244b3e71e4b606fbf80cf4936f39d86da801d6ec
Lenin Lemus <lemus@upv.es>
2025-03-06 03:57:04 +0100

    20250306 Página giirobrin

--- main/main.c
+++ main/main.c
@@ -26,8 +26,10 @@
 
 /** Página web */
 char template_index_html[4096];
+char template_giirobrin_html[4096];
 char response_data[4096];
 #define TEMPLATE_INDEX_HTML_PATH "/storage/index.html"
+#define RIN_HTML_PATH "/storage/rin.html"
 
 //char on_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\" ></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> ON</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
 //char off_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\"></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> OFF</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
@@ -158,43 +160,60 @@ esp_err_t send_web_page(httpd_req_t *req){
     response = httpd_resp_send(req, response_data, HTTPD_RESP_USE_STRLEN);
     return response;
 }
-esp_err_t get_req_handler(httpd_req_t *req){
+esp_err_t send_web_page_giirobrin(httpd_req_t *req){
+    int response;
+    sprintf(response_data, template_giirobrin_html, "2025");
+    response = httpd_resp_send(req, response_data, HTTPD_RESP_USE_STRLEN);
+    return response;
+}
+esp_err_t get_req_handler(httpd_req_t *req) {
     return send_web_page(req);
 }
 
-esp_err_t led_on_handler(httpd_req_t *req)
-{
+esp_err_t led_on_handler(httpd_req_t *req) {
     gpio_set_level(LED_PIN, 1);
     led_state = 1;
     ESP_LOGE(TAG, "ON");
     return send_web_page(req);
 }
 
-esp_err_t led_off_handler(httpd_req_t *req)
-{
+esp_err_t led_off_handler(httpd_req_t *req) {
     gpio_set_level(LED_PIN, 0);
     led_state = 0;
     ESP_LOGE(TAG, "OFF");
     return send_web_page(req);
 }
+esp_err_t giirobrin_handler(httpd_req_t *req) {
+    return send_web_page_giirobrin(req);
+}
+
 
 httpd_uri_t uri_get = {
     .uri = "/",
     .method = HTTP_GET,
     .handler = get_req_handler,
-    .user_ctx = NULL};
+    .user_ctx = NULL
+};
 
 httpd_uri_t uri_on = {
     .uri = "/led2on",
     .method = HTTP_GET,
     .handler = led_on_handler,
-    .user_ctx = NULL};
+    .user_ctx = NULL
+};
 
 httpd_uri_t uri_off = {
     .uri = "/led2off",
     .method = HTTP_GET,
     .handler = led_off_handler,
-    .user_ctx = NULL};
+    .user_ctx = NULL
+};
+httpd_uri_t uri_giirobrin = {
+    .uri = "/giirobrin",
+    .method = HTTP_GET,
+    .handler = giirobrin_handler,
+    .user_ctx = NULL
+};
 
 httpd_handle_t setup_server(void)
 {
@@ -206,6 +225,7 @@ httpd_handle_t setup_server(void)
         httpd_register_uri_handler(server, &uri_get);
         httpd_register_uri_handler(server, &uri_on);
         httpd_register_uri_handler(server, &uri_off);
+        httpd_register_uri_handler(server, &uri_giirobrin);
     }
 
     return server;
@@ -219,7 +239,7 @@ void setupSpiffs(){
     esp_vfs_spiffs_conf_t config = {
         .base_path = "/storage",
         .partition_label = NULL,
-        .max_files = 5,
+        .max_files = 8,
         .format_if_mount_failed = true
     };
     esp_err_t result = esp_vfs_spiffs_register(&config);
@@ -238,21 +258,9 @@ void setupSpiffs(){
 }
 
 /** Index.html */
-
-
 void readIndexHtml(){
-   /*  FILE* f = fopen("/storage/myfile.txt", "r");
-    if (f == NULL) {
-        ESP_LOGE(TAG, "Failed to open file for reading");
-        return;
-    }
-    char line[64];
-    fgets(line, sizeof(line), f);
-    fclose(f);
-    printf("%s\n", line);
-
-    ESP_LOGI(TAG, "File content %s\n", line); 
-    */
+    
+    
     memset((void *) template_index_html, 0, sizeof(template_index_html));
     struct stat st;
     if (stat(TEMPLATE_INDEX_HTML_PATH, &st)){
@@ -262,12 +270,42 @@ void readIndexHtml(){
 
     FILE *fp = fopen(TEMPLATE_INDEX_HTML_PATH, "r");
     if (fread(template_index_html, st.st_size, 1, fp) == 0){
-        ESP_LOGE(TAG, "fread failed");
+        ESP_LOGE(TAG, "fread failed index.html");
     }
     fclose(fp);
 }
+
+void readGIIROBRINHtml(){
+   /*
+    FILE* f = fopen("/storage/giirobrin.html", "r");
+    if (f == NULL) {
+        ESP_LOGE(TAG, "Failed to open file for reading");
+        return;
+    }
+    char buffer[4096];
+    fgets(buffer, sizeof(buffer), f);
+    fclose(f);
+    printf("%s\n", buffer);
+
+    ESP_LOGE(TAG, "File content %s\n", buffer);  
+    */
+    
+    memset((void *) template_giirobrin_html, 0, sizeof(template_giirobrin_html));
+    struct stat st;
+    if (stat(RIN_HTML_PATH, &st)){
+        ESP_LOGE(TAG, "giirobrin.html not found");
+        return;
+    }
+
+    FILE *fp = fopen(RIN_HTML_PATH, "r");
+    if (fread(template_giirobrin_html, st.st_size, 1, fp) == 0){
+        ESP_LOGE(TAG, "fread failed giirobrin.html");
+    }
+    fclose(fp);
+
+}
 /***********************************************************
- * 
+ * Main function
  ***********************/
 void app_main()
 {
@@ -282,6 +320,8 @@ void app_main()
 
     // index page
     setupSpiffs();
+    
+    readGIIROBRINHtml();
     readIndexHtml();
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
